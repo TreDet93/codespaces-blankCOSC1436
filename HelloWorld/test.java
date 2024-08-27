2e2615fd9f1546aecdfe37cf99083bf16361189f
TreDet93 <deterville.n519873@stu.sanjac.edu>
2024-08-27 22:42:00 +0000

    Hello

---
+++ HelloWorld/test.java
@@ -0,0 +1,21 @@
+public class HelloWorld
+public static void main(string [] args)
+{
+    system.out.println('This is a test of the boradcast system');
+
+} Class 'HelloWorld';
+My name is Tre Deterville
+My favorite food is tacos
+A celebrity whom I would like to meet is Lebron James
+My least favorite chore at home is to fold clothes
+One thing I would rather be doing than doing this assignment is traveling
+The first line used in code to to create the class
+The second line is used to create the static
+The third is to test the program
+And the rest of the is used for the output
+The main() represents the starting point in which the JVM starts the execution
+The system.out.println is used for the output command
+The comments is used to help humans better understand code
+
+
+
