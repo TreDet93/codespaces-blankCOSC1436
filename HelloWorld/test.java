5d26002a18168dd1897712acb7b79e5f31bee050
TreDet93 <deterville.n519873@stu.sanjac.edu>
2024-08-28 01:05:19 +0000

    new commit

--- HelloWorld/test.java
+++ HelloWorld/test.java
@@ -1,21 +1,24 @@
-public class HelloWorld
-public static void main(string [] args)
+public class test
 {
-    system.out.println('This is a test of the boradcast system');
+    public static void main(String [] args)
+    {
+        System.out.println("This is a test of the boradcast system");
+        System.out.println("My name is Tre Deterville");
+        System.out.println("My favorite food is tacos");
+        System.out.println("A celebrity whom I would like to meet is Lebron James\n" +
+        "My least favorite chore at home is to fold clothes\n" +
+        "One thing I would rather be doing than doing this assignment is traveling");
 
-} Class 'HelloWorld';
-My name is Tre Deterville
-My favorite food is tacos
-A celebrity whom I would like to meet is Lebron James
-My least favorite chore at home is to fold clothes
-One thing I would rather be doing than doing this assignment is traveling
-The first line used in code to to create the class
+    }
+}
+/*The first line used in code to to create the class
 The second line is used to create the static
 The third is to test the program
 And the rest of the is used for the output
 The main() represents the starting point in which the JVM starts the execution
 The system.out.println is used for the output command
-The comments is used to help humans better understand code
+The comments is used to help humans better understand code*/
+
 
 
 
