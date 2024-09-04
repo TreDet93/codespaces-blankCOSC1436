b892a24138803590dbc73ea22bb2c58284d331c6
TreDet93 <deterville.n519873@stu.sanjac.edu>
2024-09-04 00:15:00 +0000

    commit

---
+++ HelloWorld/BudgetCalculatorProject.java
@@ -0,0 +1,38 @@
+public class BudgetCalculatorProject
+{    
+public static void main(String [] args) 
+{
+    String a = SALARY; // The fixed month salary
+    String b = SAVINGS_PERCENT ; // The percentage of salary saved
+    String c = RENT_PERCENT; // The percentage of salary spent on rent
+    String d = GROCERIES_PERCENT; // The percentage of salary spent on groceries 
+    String e = ENTERTAINMENT_PERCENT; // The percentage spent on entertainment
+
+    
+    System.out.println(name + "monthlySalary"); 
+    System.out.println(name + "savedAmount"); 
+    System.out.println(name + "rentAmount"); 
+    System.out.println(name + "groceriesAmount");
+    System.out.println(name + "entertainmentAmount");
+
+    monthlySalary= 3000.0;
+    savedAmount= 0.20;
+    rentAmount = 0.30; 
+    groceriesAmount= 0.15;
+    entertainmentAmount = 0.10;
+
+    int  monthlySalary; (a)= 3000.0;
+    int  savedAmount; (b)= 0.20;
+    int  rentAmount; (c)= 0.10; 
+    int  groceriesAmount; (d)=0.30;
+    int entertainmentAmount; (e)= 0.15; 
+    totalExpenses= 0.65;
+
+    
+    
+
+
+
+}
+}
+
