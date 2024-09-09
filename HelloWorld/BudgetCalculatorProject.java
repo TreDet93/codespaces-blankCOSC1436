6bb0e9c9d378b26752a24d67cdab247304f751a5
TreDet93 <deterville.n519873@stu.sanjac.edu>
2024-09-09 00:38:11 +0000

    first commit

--- HelloWorld/BudgetCalculatorProject.java
+++ HelloWorld/BudgetCalculatorProject.java
@@ -1,38 +1,45 @@
+import java.util.Scanner;
+
 public class BudgetCalculatorProject
 {    
-public static void main(String [] args) 
-{
-    String a = SALARY; // The fixed month salary
-    String b = SAVINGS_PERCENT ; // The percentage of salary saved
-    String c = RENT_PERCENT; // The percentage of salary spent on rent
-    String d = GROCERIES_PERCENT; // The percentage of salary spent on groceries 
-    String e = ENTERTAINMENT_PERCENT; // The percentage spent on entertainment
+    public static void main(String [] args) 
+    {
+        final int SALARY = 3000; // The fixed month salary
+        final double SAVINGS_PERCENT = 0.2 ; // The percentage of salary saved
+        final double  RENT_PERCENT = 0.3; // The percentage of salary spent on rent
+        final double  GROCERIES_PERCENT =0.15; // The percentage of salary spent on groceries 
+        final double  ENTERTAINMENT_PERCENT = 0.10; // The percentage spent on entertainment
 
-    
-    System.out.println(name + "monthlySalary"); 
-    System.out.println(name + "savedAmount"); 
-    System.out.println(name + "rentAmount"); 
-    System.out.println(name + "groceriesAmount");
-    System.out.println(name + "entertainmentAmount");
-
-    monthlySalary= 3000.0;
-    savedAmount= 0.20;
-    rentAmount = 0.30; 
-    groceriesAmount= 0.15;
-    entertainmentAmount = 0.10;
-
-    int  monthlySalary; (a)= 3000.0;
-    int  savedAmount; (b)= 0.20;
-    int  rentAmount; (c)= 0.10; 
-    int  groceriesAmount; (d)=0.30;
-    int entertainmentAmount; (e)= 0.15; 
-    totalExpenses= 0.65;
-
-    
-    
+        double monthlySalary= SALARY;
+        System.out.println("monthly salary: " + monthlySalary);
+        
+        double savedAmount = monthlySalary * SAVINGS_PERCENT;
+        System.out.println(savedAmount); 
 
 
+        System.out.println(name + "rentAmount"); 
+        System.out.println(name + "groceriesAmount");
+        System.out.println(name + "entertainmentAmount");
 
-}
+        
+        savedAmount= 0.20;
+        rentAmount = 0.30; 
+        groceriesAmount= 0.15;
+        entertainmentAmount = 0.10;
+
+        /*
+        int  monthlySalary; (a)= 3000.0;
+        int  savedAmount; (b)= 0.20;
+        int  rentAmount; (c)= 0.10; 
+        int  groceriesAmount; (d)=0.30;
+        int entertainmentAmount; (e)= 0.15; 
+        totalExpenses= 0.65;
+        */
+        
+        
+
+
+
+    }
 }
 
