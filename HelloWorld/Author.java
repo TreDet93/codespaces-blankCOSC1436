0a73d69f3ce15364c1b7eb4e53d8bfb0cc824ae1
TreDet93 <deterville.n519873@stu.sanjac.edu>
2024-09-23 03:27:17 +0000

    third commit

---
+++ HelloWorld/Author.java
@@ -0,0 +1,54 @@
+public class Author {
+    //Class Attributes
+    public String name;
+    public String email;
+    public String nationality;
+
+ /**
+   * 
+   * Default Constructor 
+   */
+  public String Book(){
+    return this.name="Tim";
+  }
+  
+  public String email(){
+    return this.email="Timothy55@yahoo.com";
+  }
+   
+  public String nationality(){
+    return this.nationality="American";
+    
+  }
+public class Book{
+public String title;
+public String price;
+public String twitter;
+}
+public String title(){
+    return this.title="How To Sail A Boat";
+}
+public String price(){
+    return this.price="27.95";
+}
+    public String authorObject(){
+        return this.twitter="@ExplorerKing";
+    }
+public class main Method(){
+    String Twitter;
+    String sailingFun;
+}
+
+}
+  
+    
+  
+
+
+
+
+
+
+
+
+}
